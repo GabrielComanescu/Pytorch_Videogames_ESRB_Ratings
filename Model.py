109492e57f1efa8a609cd19d13e743d2bee6a16b
GabrielComanescu <37899015+GabrielComanescu@users.noreply.github.com>
2021-03-04 21:23:54 +0200

    Add files via upload

--- Model.py
+++ Model.py
@@ -8,14 +8,12 @@ class CoolModel(nn.Module):
 
         self.fc1 = nn.Linear(in_features, h1)
         self.fc2 = nn.Linear(h1, h2)
-        # self.fc3 = nn.Linear(h2, h3)
         self.fc3 = nn.Linear(h2, out_features)
 
     def forward(self, x):
 
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        # x = F.relu(self.fc3(x))
         x = self.fc3(x)
 
         return F.log_softmax(x, dim=1)
